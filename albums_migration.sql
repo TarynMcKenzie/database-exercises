aa5c1fa964c286ca5879e571a1e646c5442f0e71
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-14 11:06:11 -0500

    Lesson walkthrough completed for basic statements

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,9 +1,9 @@
-\W
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
+
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) DEFAULT 'NONE',
     name  VARCHAR(100) NOT NULL,
@@ -11,4 +11,5 @@ CREATE TABLE albums (
     sales FLOAT NOT NULL,
     genre VARCHAR(500) DEFAULT 'NONE',
     PRIMARY KEY (id)
+
 );
---
+++ quotes_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS quotes;
+
+CREATE TABLE quotes (
+
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    author_first_name VARCHAR(50) DEFAULT 'NONE',
+    author_last_name  VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    PRIMARY KEY (id)
+
+);
