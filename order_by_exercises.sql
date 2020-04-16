b2ad4501fca0fc3bfa9d626563a738a78f071695
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-15 21:26:55 -0500

    2/3 exercises completed for the mysql clauses modules

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -31,14 +31,41 @@ SELECT first_name, last_name FROM employees
 WHERE last_name LIKE '%q%'
   AND NOT last_name LIKE '%qu%';
 
--- TODO: 2. Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
 
--- TODO: 3. Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+-- TODO: 2. Modify your first query to order by first name.
+--          The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
 
--- TODO: 4. Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 
--- TODO: 5. Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+
+-- TODO: 3. Update the query to order by first name and then last name.
+--          The first result should now be Irena Acton and the last should be Vidya Zweizig.
+
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name ASC, last_name ASC;
+
+
+-- TODO: 4. Change the order by clause so that you order by last name before first name.
+--          Your first result should still be Irena Acton but now the last result should be Maya Zyda.
+
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name ASC, first_name ASC;
+
+
+-- TODO: 5. Update your queries for employees with 'E' in their last name to sort the results by their employee number.
+--          Your results should not change!
+
+SELECT last_name FROM employees WHERE last_name LIKE 'e%'ORDER BY emp_no ASC;
+
 
 -- TODO: 6. Now reverse the sort order for both queries.
 
--- TODO: 7. Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
+
+SELECT last_name FROM employees WHERE last_name LIKE 'e%'ORDER BY emp_no DESC;
+
+-- TODO: 7. Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last.
+--          It should be Khun Bernini.
+
+SELECT first_name, last_name, hire_date, birth_date FROM employees
+WHERE hire_date LIKE '%199%'
+  AND birth_date LIKE '%12-25%'
+ORDER BY birth_date ASC, hire_date DESC;
