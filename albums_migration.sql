43a95074f0e2c61804a3f2db4942e325732ee150
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-16 16:31:43 -0500

    Indexes exercise complete, walkthrough also completed

--- albums_migration.sql
+++ albums_migration.sql
@@ -10,6 +10,14 @@ CREATE TABLE albums (
     release_date INT(4) UNSIGNED NOT NULL,
     sales FLOAT NOT NULL,
     genre VARCHAR(500) DEFAULT 'NONE',
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (name, artist)
 
 );
+
+-- INDEXES SECTION EXERCISES
+-- TODO: 1. USE your employees database.
+-- TODO: 2. DESCRIBE each table and inspect the keys and see which columns have indexes and keys.
+
+-- TODO: 3. USE your codeup_test_db database.
+-- TODO: 4. Add an index to make sure all album names combined with the artist are unique. Try to add duplicates to test the constraint.
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -8,7 +8,7 @@
 
 USE codeup_test_db;
 
---Truncate all records before re seeding new data
+-- Truncate all records before re seeding new data
 TRUNCATE albums;
 
 
@@ -59,4 +59,6 @@ VALUES          ('Michael Jackson', 'Thriller', '1982', 'Pop/Rock/R&B', '47.3'),
                 ('Nirvana', 'Nevermind', '1991', 'Grunge/Alternative rock', '16.7'),
                 ('Pink Floyd', 'The Wall', '1979', 'Progressive rock', '17.6'),
                 ('Santana', 'Supernatural', '1999', 'Rock', '20.5'),
+#                 ('Guns N Roses', 'Appetite for Destruction', '1987', 'Hard rock', '21.6'),
                 ('Guns N Roses', 'Appetite for Destruction', '1987', 'Hard rock', '21.6');
+
